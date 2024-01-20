2a6f408f6dea88028adf8cb87b0d5e939892f6d3
Madan Malhotra <starthecode@gmail.com>
2024-01-20 18:12:44 +0530

    bug fix

--- src/app/(root)/(dash)/users/page.tsx
+++ src/app/(root)/(dash)/users/page.tsx
@@ -5,8 +5,6 @@ import React from 'react';
 export default async function Users() {
   const users = await getUsers();
 
-  console.log(users);
-
   return (
     <section>
       <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
