b92b8520267718dd7a4f559487e1e562c30d2fde
Madan Malhotra <starthecode@gmail.com>
2024-01-20 18:16:20 +0530

    bug fix

--- src/app/(root)/(dash)/products/add/page.tsx
+++
@@ -1,8 +0,0 @@
-import ProductForm from '@/components/products/ProductForm';
-import ProductSidebar from '@/components/products/ProductSidebar';
-import React from 'react';
-
-export default function AddProduct() {
-  const product: any = [];
-  return <ProductForm type="Add" product={product} />;
-}
